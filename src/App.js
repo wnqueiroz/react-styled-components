f7cacbb920b552d301a36baf173363e064a0f2ea
William Queiroz <wnqueiroz@outlook.com>
2018-10-15 18:56:37 -0300

    Refactoring to use styled-components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,70 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+
+import styled, { keyframes } from 'styled-components'
+
+import logo from './logo.svg'
+
+const AppWrapper = styled.div`
+  text-align: center;
+`
+
+const rotate360 = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`
+
+const AppLogo = styled.img`
+  animation: ${rotate360} infinite 120s linear;
+  height: 80px;
+  &:hover {
+    animation: ${rotate360} infinite 1.5s linear;
+  }
+`
+
+const AppHeader = styled.div`
+  background-color: #222;
+  height: 12rem;
+  padding: 1rem;
+  color: white;
+`
+
+const AppTitle = styled.h1`
+  font-weight: 900;
+`
+
+const AppIntro = styled.p`
+  font-size: large;
+  code {
+    font-size: 1.3rem;
+  }
+`
+
+const EmojiWrapper = styled.span.attrs({
+  role: 'img',
+})``
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <AppWrapper>
+        <AppHeader>
+          <AppLogo src={logo} alt="logo" />
+          <AppTitle>Welcome to React</AppTitle>
+        </AppHeader>
+        <AppIntro>
+          Bootstrapped with <code>create-react-app</code>.
+        </AppIntro>
+        <AppIntro>
+          Components styled with <code>styled-components</code>{' '}
+          <EmojiWrapper aria-label="nail polish" />
+        </AppIntro>
+      </AppWrapper>
+    )
   }
 }
 
-export default App;
+export default App
